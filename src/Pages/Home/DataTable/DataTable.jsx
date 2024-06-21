eb5f47ac2f45f994cc535086d5b3c5610b15585e
Saiful Islam <saifulislamsaif797@gmail.com>
2024-06-21 14:38:34 +0600

    Basic setup done for client site for JW-Basic-Project

---
+++ src/Pages/Home/DataTable/DataTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DataTable = () => {
+    return (
+        <div>
+            This is table.
+        </div>
+    );
+};
+
+export default DataTable;
---
+++ src/Pages/Home/HomePage.jsx
@@ -0,0 +1,10 @@
+
+const HomePage = () => {
+    return (
+        <div>
+            <p className='text-4xl font-bold text-center text-yellow-400'>This is my home page</p>
+        </div>
+    );
+};
+
+export default HomePage;
